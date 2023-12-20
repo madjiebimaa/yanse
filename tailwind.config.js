2dca905d6c927f3a998c670ba4275fc3f6c4fd80
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2023-12-20 16:15:23 +0700

    style: add fonts and favicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        xano: ['Xano Mincho', 'sans-serif'],
+        bpt: ['Bptypewrite', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
